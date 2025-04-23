4d381fe95ceb956f070283a368b653f8eb122a23
Cameron Hyden <camh.hyden@gmail.com>
2025-04-23 15:24:40 +0100

    tigger build

--- sousChefAi/Program.cs
+++ sousChefAi/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
